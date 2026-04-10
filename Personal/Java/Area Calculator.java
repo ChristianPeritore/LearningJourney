c5a81a931f7998e03a98288da68365be77e7fda3
Christian Peritore <christian.peritore@rapisardidavinci.it>
2026-04-10 17:51:02 +0200

    Create Area Calculator.java

---
+++ Personal/Java/Area Calculator.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		double base = 7.6, height = 8.1;
+		double area;
+		area = base * height;
+		System.out.println("The area of the rectangule with " + base + " as the base and " + height + " as the height is " + area);
+	}
+}
