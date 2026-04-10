5105e9ad4c8e72da59137eff36a41a88633eac08
Christian Peritore <christian.peritore@rapisardidavinci.it>
2026-04-10 18:43:53 +0200

    Create The Smart Discount.java

---
+++ Personal/Java/The Smart Discount.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Hi, please enter your age");
+		int age = keyboard.nextInt();
+		double discount = 0;
+		if (age < 16) {
+			System.out.println("Sorry, you're too young to enter this bar.");
+		} else {
+			keyboard.nextLine();
+			System.out.println("Hi, welcome in my bar! Enter a product, please (if you want more than one, enter the plural form of the product");
+			String nameProduct = keyboard.nextLine();
+			System.out.println("Now enter the price of only one");
+			double price = keyboard.nextDouble();
+			System.out.println("How many do you want?");
+			int numb = keyboard.nextInt();
+			double tot = price * numb;
+			double iva = tot * 0.22; // Italian IVA = Imposta sul Valore Aggiunto (tax on products) ---> 22%
+			tot = tot + iva;
+			if (tot > 50) {
+				discount = tot * 0.10;
+				tot = tot - discount;
+				System.out.println("You riceved a 10% discount!");
+			} else if (tot > 20) {
+				discount = tot * 0.05;
+				tot = tot - discount;
+				System.out.println("You riceved a 5% discount!");
+			}
+			System.out.println("You ordered " + numb + " " + nameProduct + ", the price is " + tot + " eur");
+		}
+	}
+}
