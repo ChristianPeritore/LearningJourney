e924f5579f5a83589e2fc091d8effbab84e91c5d
Christian Peritore <christian.peritore@rapisardidavinci.it>
2026-04-10 18:31:25 +0200

    Update Simple Receipt Generator.java

--- Personal/Java/Simple Receipt Generator.java
+++ Personal/Java/Simple Receipt Generator.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		Scanner kewboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Hi, please enter your age");
+		int age = keyboard.nextInt();
+		if (age < 16) {
+		    System.out.println("Sorry, you're too young to enter this bar.");
+		}
+		keyboard.nextLine();
 		System.out.println("Hi, welcome in my bar! Enter a product, please (if you want more than one, enter the plural form of the product");
-		String nameProduct = kewboard.nextLine();
+		String nameProduct = keyboard.nextLine();
 		System.out.println("Now enter the price of only one");
-		double price = kewboard.nextDouble();
+		double price = keyboard.nextDouble();
 		System.out.println("How many do you want?");
-		int numb = kewboard.nextInt();
+		int numb = keyboard.nextInt();
 		double tot = price * numb;
 		double iva = tot * 0.22; // Italian IVA = Imposta sul Valore Aggiunto (tax on products) ---> 22%
 		tot = tot + iva;
