bc45e2a6f4f3c0042f9ebacbe9e292888fde7aff
Christian Peritore <christian.peritore@rapisardidavinci.it>
2026-04-10 17:46:39 +0200

    Create User Intro.java

---
+++ Personal/Java/User Intro.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "Chrstian";
+        int age = 16;
+        String city = "Caltanissetta";
+        System.out.println("Hi, I'm " + name + ", I am " + age + " years old and I'm from " + city);
+    }
+}
