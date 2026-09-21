e98615ad835563582d09577386e9a5c6dcb2baf2
Christian Peritore <chri.pe09@gmail.com>
2026-09-21 19:12:27 +0200

    Add function to demonstrate pass-by-reference

---
+++ School/C++/functionWithPass-By-Reference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int sum(int a, int b, int& c) {
+    c = a + b;
+    return c;
+}
+int main() {
+    int a = 10;
+    int b = 7;
+    int c = 0;
+    sum(a, b, c); 
+    cout << "C is " << c << endl;
+    return 0;
+}
